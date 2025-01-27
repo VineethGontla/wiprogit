53e691f818456426fa50cf87f8da5a89548a564c
Gontla Vineeth <gontlavineeth2@gmail.com>
2025-01-27 14:12:56 +0530

    firstfile

---
+++ task1.java
@@ -0,0 +1,15 @@
+package gittask1;
+
+public class task1 {
+	public static void main(String a[]) {
+		int num1 = 10;
+		int num2 = 20;
+		int result = num1 + num2;
+		int result1 = num1 - num2;
+		System.out.println(result);
+		System.out.println(result1);
+
+		
+	}
+
+}
