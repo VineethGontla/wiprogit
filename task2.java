f06610135ee491abc15453518aee3cb480431a3b
Gontla Vineeth <gontlavineeth2@gmail.com>
2025-01-27 14:21:04 +0530

    task2

---
+++ task2.java
@@ -0,0 +1,15 @@
+package gittask1;
+
+public class task2 {
+	
+	public static void main(String a[]) {
+		int x = 61;
+		int y = 11;
+		int res = x * y;
+		int res1 = x % y;
+		
+		System.out.println(res);
+		System.out.println(res1);
+	}
+
+}
